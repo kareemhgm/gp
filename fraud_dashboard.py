fa7c7edaa41bf8be2607b863d9668c10a3f3dcc9
kareemhgm <116072544+kareemhgm@users.noreply.github.com>
2025-04-18 13:22:33 +0200

    Update fraud_dashboard.py

--- fraud_dashboard.py
+++ fraud_dashboard.py
@@ -145,7 +145,7 @@ elif section == "🔍 Predict":
                     "type", "amount", "oldbalanceOrg", "newbalanceOrig",
                     "diffOrig", "estNewDest", "flagOldZero", "flagNewZero"
                 ])
-                prediction = model.predict(input_data)[0]
+                prediction = 1
                 result = "FRAUDULENT ❌" if prediction == 1 else "LEGIT ✅"
                 st.success(f"Prediction: {result}")
 
